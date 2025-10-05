f246d168fb99d6c431a3dabaa83217673c75d820
valecatdog <valentinapaulacorrea09@gmail.com>
2025-10-05 20:28:05 -0300

    estructura de ifs para parametros

--- AdmUsuarios.sh
+++ AdmUsuarios.sh
@@ -1,2 +1,21 @@
 #! /bin/bash
-echo "holi"
+
+if (($# == 0))
+#para 0 parametros
+then
+#aca se escribe lo que se hace (asi para todos)
+
+
+elif (($# == 1))
+#para un parametro (yo)
+then
+
+
+elif (($# == 2))
+#para dos parametros (vos)
+then
+
+
+else
+    echo "Se ha ingresado una cantidad invalida de parametros"
+fi
