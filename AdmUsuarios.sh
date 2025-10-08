0db9a37c8886ceb7a924618a17c08f0b691bb164
valecatdog <valentinapaulacorrea09@gmail.com>
2025-10-08 17:43:08 -0300

    Update AdmUsuarios.sh
    algunos arreglos

--- AdmUsuarios.sh
+++ AdmUsuarios.sh
@@ -142,6 +142,7 @@ then
 elif (($# == 2))
 nombre=$1
 apellido=$2
+usuario=$(generar_username "$nombre" "$apellido")
 
 echo "1. Añadir usuario"
 echo "2. Borrar usuario"
@@ -149,19 +150,14 @@ read -p "Elige una opción: " opcion
 
 if (( "$opcion" = 1 ))
 then
-
-usuario=$(generar_username "$nombre" "$apellido")
-    echo "Nombre de usuario generado: $usuario"
-
-        if usuario_existe "$usuario"; then
-            echo "Ya hay un usuario registrado con ese username: $usuario"
-        else
-
-        # añade el usuario como nuevo system user junto con su directorio de usuario 
+    if usuario_existe "$usuario"; then
+        echo "Ya hay un usuario registrado con el username: $usuario"
+    else
+        #añade el usuario como nuevo system user junto con su directorio de usuario 
         # -c para comment, para poder dividir nombre y apellido en diferentes campos (para luego compararlos con lo del nombre + apellido)
-            sudo useradd -m -c "$nombre $apellido" "$usuario"
-            echo "usuario '$usuario' creado correctamente"
-        fi
+        sudo useradd -m -c "$nombre $apellido" "$usuario"
+        echo "usuario '$usuario' creado correctamente"
+    fi
 
 elif (( "$opcion" = 2 ))
 then
@@ -198,3 +194,5 @@ usuario_existe() {
         # -q = quiet (no imprime mada) # ^ inicio de linea 
         grep -q "^${usuario}:" /etc/passwd
 }
+
+#HAY QUE HACER LAS FUNCIONES PARA CREAR Y BORRAR
