137a856727428d28c7017969b3419cffb39454ff
valecatdog <valentinapaulacorrea09@gmail.com>
2025-10-19 14:29:48 -0300

    cero que ahora asi

--- AdmUsuarioPrueba3.sh
+++ AdmUsuarioPrueba3.sh
@@ -7,25 +7,11 @@ archivo="$1"
 
     until [ "$valido" ]
     do
-        if [ -f "$archivo" ]
-        #velifica que "archivo" sea un archivo valido (existente)
-        #PODRIA VERIFICAR SI LA ESTRUCTURA ES VALIDA TAMMBIEN!!! (ESTA PERO NO SE SI ESTA ANDANDO)
+        if [ -f "$archivo" ] && [ -r "$archivo" ] && [ "$(wc -w < "$archivo")" -lt 2 ]
+        #velifica que "archivo" sea un archivo valido (existente, legible y que contenga 2 o mas palabras (nomb y apell))
         then
-            #si es valido
-            echo "Archivo encontrado."
-    
-            #le pongo comillas por las dudas de que me de mas de una palabra (no deberia). Es una buena practica.
-            if [ "$(wc -w < "$archivo")" -lt 2 ]
-            then
-                echo "Error: archivo invalido. Los archivos tienen que tener por lo menos dos palabras (nombre y apellido)"
-                read -rp "Ingrese una ruta válida: " archivo
-                valido=true
-                #detiene el until
-            else
-                echo "Archivo valido"
-            fi
+            echo "Archivo valido"
         else
-            #si no es valido
             echo "Error: archivo invalido o no encontrado"
             read -rp "Ingrese una ruta válida: " archivo
         fi
