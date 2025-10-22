12e3c660079dbacbfbd26557c43c9cea5c982526
valecatdog <valentinapaulacorrea09@gmail.com>
2025-10-22 00:02:05 -0300

    esta terminado????

--- AdmUsuarioPrueba1.sh
+++ AdmUsuarioPrueba1.sh
@@ -27,18 +27,18 @@ ctrl c ya podes salir
 
 #ESPACIO PARA FUNCIONES
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!HAY QUE CAMBIAR ESTA FUNCION, YA NO SIRVE AMS
-generar_data() {
+generar_usuario() {
     local nombre
     local apellido
-    local usuario
+    local user
     local primeraLetra
     #se hace por separado porque al ponerle local de una se pierde el valor de retorno ($?, si es 0, 1 etc)
 
     nombre="$1"
     apellido="$2"
     primeraLetra=$(echo "$nombre" | cut -c1)
-    usuario="$primeraLetra$apellido"
-    data="${nombre}:${apellido}:$usuario"
+    user="$primeraLetra$apellido"
+    usuario="${nombre}:${apellido}:$user"
     #parece qeu no se usa, pero mas adelante si se usa
 }
 
@@ -294,7 +294,31 @@ then
     done
 #############################################ESTA PARTE ESTA CORRECTA############################################
 #SI SE INGRESA UN PARAMETRO (ARREGLAR)
+elif (($# == 2))
+then
+    valido=false
+    until [ "$valido" = true ]
+    do
+        generar_usuario "$1" "$2"
 
+        echo "Que desea hacer?"
+        echo "1. Crear usuario"
+        printf "2. Eliminar usuario del sistema\n"
+        read -rp "Elija una opción: " opcion
+
+        if (( "$opcion" == 1 )) 2>/dev/null; then
+        #mando el error a /dev/null porque pode ingresar cosas no numericas y te tira error, pero funciona bien
+            valido="true"
+            add_usuario "$usuario"
+        elif (( "$opcion" == 2 )) 2>/dev/null; then
+            valido="true"
+            del_usuario "$usuario"
+        else
+            printf "\n----------------------------\n\n"
+            echo "Error: opcion invalida"
+            printf "\n----------------------------\n"
+        fi
+    done
 
 
 else
