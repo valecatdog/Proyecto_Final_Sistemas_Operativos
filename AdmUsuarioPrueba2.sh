0f47e7815db61863a1a6fb8e51b303342b00e629
valecatdog <valentinapaulacorrea09@gmail.com>
2025-10-26 20:09:09 -0300

    www

--- AdmUsuarioPrueba2.sh
+++ AdmUsuarioPrueba2.sh
@@ -345,7 +345,7 @@ gestion_usuarios(){
                         :
                         #una forma de decir no hacer nada
                     else
-                        echo "ERROR: formato de nombres incorrecto"
+                        print -n1 -t1 -rsp "Error: formato de nombres incorrecto"
                     fi
                 ;;  
             3)
