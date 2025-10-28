c0e046f71f48e5514f19b4f53cfd5e1eed6c6a90
hexal <hexala1234@gmail.com>
2025-10-28 12:43:52 -0300

    debug backups.sh #2

--- backup.sh
+++ backup.sh
@@ -326,7 +326,7 @@ while true; do
             ;;
         0)
              echo "cerrando programa"
-             false
+             exit 0 
             ;;
         *)
             echo "Opción inválida"
