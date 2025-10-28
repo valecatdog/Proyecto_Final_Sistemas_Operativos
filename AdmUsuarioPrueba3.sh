6312330db68cc2cc2301fe78bd4e7d1f52862f70
valecatdog <valentinapaulacorrea09@gmail.com>
2025-10-28 19:39:37 -0300

    mepicalaoreja

--- AdmUsuarioPrueba3.sh
+++ AdmUsuarioPrueba3.sh
@@ -19,6 +19,78 @@ se le podria agregar la opcion de ponerle contraseña a un gruó
 ayuda no me da la cabeza para mas
 '
 
+add_usuario(){
+    echo "DEBUG: parametro que le llego (toda la info): $1"
+    #creamos las variables y las hacemos locales (solo existen para esta funcion y se resetean cada vez que actua)
+    local user
+    local nombre
+    local apellido
+    local letraNombre
+    local letraApellido
+    local passwd
+
+    # extraemos los datos del usuario (almacenados como nombre:apellido:usuario)
+    nombre="$(echo "$1" | cut -d: -f1)"
+    apellido="$(echo "$1" | cut -d: -f2)"
+    user="$(echo "$1" | cut -d: -f3)"
+    read -rt3 -n1 -p "DEBUG: nombre $nombre apellido $apellido user $user "
+
+    #verifico la salida de la funcion, si es distinta a 0 (no se encontró en /etc/passwd asi que no existe) actua
+    #le pasamos el primer parametro que se le paso a la funcion actual
+    if ! usuario_existe "$1" #probar cambiar esto
+    then
+        #generar contraseña
+        letraNombre=$(echo "$nombre" | cut -c1 | tr '[:lower:]' '[:upper:]')
+        #extraemos la primera letra del nombre (como antes) y si esta en minuscula la pasamos a mayuscula
+        letraApellido=$(echo "$apellido" | cut -c1 | tr '[:upper:]' '[:lower:]')
+        #extraemos la primera letra del apellido (como antes) y si esta en mayuscula la pasamos a minuscula
+        passwd="$letraNombre${letraApellido}#123456"
+        #la contraseña va a se la letraNombre+letraApellido+#123456 (como pide la consigna)
+
+        #ingresar usuario
+        sudo useradd -mc "$nombre $apellido" "$user"
+        : 'aniadimos el usuario con useradd. -m crea el directorio del usuario si no existe y
+        -c agrema un comentario (nombre apellido). aunque el script deberia ser ejecutado con sudo, en caso
+        de olvido, lo agregamos de todas formas 
+        '
+        echo "$user":"$passwd" | sudo chpasswd 
+        #chpasswd, que asigna contrasenias, espera recibir parametros por entrada estandar. por eso el pipe
+        sudo chage -d 0 "$user"
+        #chage -d establece a fecha del ultimo cambio de la contrasenia, y 0 hace qeu expire inmediatamente
+
+        read -n1 -t2 -rsp "Usuario $user creado correctamente. Contraseña: $passwd"
+        ingreso_usuario "$nombre" "$apellido"
+        return
+        #mensaje para informar que el usuario se creo exitosamente
+        
+    else
+        read -n1 -t3 -rsp "Error: el usuario $user ($nombre $apellido) ya existe en el sistema"
+        : 'informa que el usuario ya existe, no se puede crear
+        -n1: acepta un caracter. sirve para que la proxima vez qeu se haga un read, lo que se escribe en este no
+        "contamine" el otro (limpia el buffer). -t1: tiempo de espera de un segundo, -r: no interpreta lo que
+        escribe el usuario. -s: no muestra lo que se escribe. -p: para mosrtar el mensaje. Como no nos interesa
+        si el usuario escribe algo no especificamos una variable para que se guarde
+        '
+        echo "$1" >> cre_usuarios.log 
+        ingreso_usuario "$nombre" "$apellido"
+        return
+        #pasamos la informacion del usuario (prierm parametro de la funcion) al log 
+    fi
+} 
+
+usuario_existe() {
+        local user
+        user="$(echo "$1" | cut -d: -f3)"
+        # -q = quiet (no imprime mada) # ^ inicio de linea 
+        #habra que escapar el $
+        getent passwd "$user" >/dev/null
+        : 'verifica si existe el usuario en passwd, si existe te imprime su info. como no qeuremos eso, lo redirigimos 
+        a /dev/null'
+}
+
+
+#-------------------------------------------
+
 add_usuario "vale:correa:vcorrea"
 add_usuario "aru:sifilis:asifilis"
 add_usuario "ayuda:help:odiotodioyatodeldo"
