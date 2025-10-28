86e1e2e376294c44624a600a43f82a9da12173e2
valecatdog <valentinapaulacorrea09@gmail.com>
2025-10-28 20:20:15 -0300

    mentira no anda

--- AdmUsuarioPrueba2.sh
+++ AdmUsuarioPrueba2.sh
@@ -212,18 +212,24 @@ generar_usuario() {
 #CORREGIDO Y COMENTADO
 #recibe usuario completo
 usuario_existe() {
-        local user
-        user="$(echo "$1" | cut -d: -f3)"
-        # -q = quiet (no imprime mada) # ^ inicio de linea 
-        #habra que escapar el $
-        getent passwd "$user" >/dev/null
-        : 'verifica si existe el usuario en passwd, si existe te imprime su info. como no qeuremos eso, lo redirigimos 
-        a /dev/null'
+    local user
+    user="$(echo "$1" | cut -d: -f3)"
+    echo "DEBUG: usuario_existe: user = $user"
+    sleep 2
+    if getent passwd "$user" >/dev/null; then
+        echo "DEBUG: getent encontró al usuario, return 0"
+        sleep 2
+        return 0
+    else 
+        echo "DEBUG: getent NO encontró al usuario, return 1"
+        sleep 2
+        return 1
+    fi
 }
 
 #CORREGIDO Y COMENTADO
 #recibe usuario completo
-add_usuario(){
+add_usuario1(){
     local user
     local nombre
     local apellido
@@ -258,6 +264,58 @@ add_usuario(){
         #return
         #mensaje para informar que el usuario se creo exitosamente
     else
+        read -n1 -t3 -rsp "Error: el usuario $user ($nombre $apellido) ya existe en el sistema"
+        : 'informa que el usuario ya existe, no se puede crear
+        -n1: acepta un caracter. sirve para que la proxima vez qeu se haga un read, lo que se escribe en este no
+        "contamine" el otro (limpia el buffer). -t1: tiempo de espera de un segundo, -r: no interpreta lo que
+        escribe el usuario. -s: no muestra lo que se escribe. -p: para mosrtar el mensaje. Como no nos interesa
+        si el usuario escribe algo no especificamos una variable para que se guarde
+        '
+        echo "$1" >> cre_usuarios.log 
+        ingreso_usuario "$nombre" "$apellido"
+        return
+    fi
+}
+add_usuario(){
+    local user
+    local nombre
+    local apellido
+
+    nombre="$(echo "$1" | cut -d: -f1)"
+    apellido="$(echo "$1" | cut -d: -f2)"
+    user="$(echo "$1" | cut -d: -f3)"
+
+    echo "DEBUG: user = $user"
+    sleep 2
+    if ! usuario_existe "$1"; then
+        echo "DEBUG: El usuario NO existe, creando..."
+        sleep 2
+        #generar contraseña
+        letraNombre=$(echo "$nombre" | cut -c1 | tr '[:lower:]' '[:upper:]')
+        #extraemos la primera letra del nombre (como antes) y si esta en minuscula la pasamos a mayuscula
+        letraApellido=$(echo "$apellido" | cut -c1 | tr '[:upper:]' '[:lower:]')
+        #extraemos la primera letra del apellido (como antes) y si esta en mayuscula la pasamos a minuscula
+        passwd="$letraNombre${letraApellido}#123456"
+        #la contraseña va a se la letraNombre+letraApellido+#123456 (como pide la consigna)
+
+        #ingresar usuario
+        sudo useradd -mc "$nombre $apellido" "$user"
+        : 'aniadimos el usuario con useradd. -m crea el directorio del usuario si no existe y
+        -c agrema un comentario (nombre apellido). aunque el script deberia ser ejecutado con sudo, en caso
+        de olvido, lo agregamos de todas formas 
+        '
+        echo "$user":"$passwd" | sudo chpasswd 
+        #chpasswd, que asigna contrasenias, espera recibir parametros por entrada estandar. por eso el pipe
+        sudo chage -d 0 "$user"
+        #chage -d establece a fecha del ultimo cambio de la contrasenia, y 0 hace qeu expire inmediatamente
+
+        read -n1 -t2 -rsp "Usuario $user creado correctamente. Contraseña: $passwd"
+        #ingreso_usuario "$nombre" "$apellido"
+        #return
+        #mensaje para informar que el usuario se creo exitosamente
+    else
+        echo "DEBUG: El usuario SÍ existe, no se crea."
+        sleep 2
         read -n1 -t3 -rsp "Error: el usuario $user ($nombre $apellido) ya existe en el sistema"
         : 'informa que el usuario ya existe, no se puede crear
         -n1: acepta un caracter. sirve para que la proxima vez qeu se haga un read, lo que se escribe en este no
