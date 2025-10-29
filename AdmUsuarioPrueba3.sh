0e01845d8a0f6e91638bb6fcf73eae74b8cc879f
valecatdog <valentinapaulacorrea09@gmail.com>
2025-10-29 19:31:09 -0300

    probando alfo

--- AdmUsuarioPrueba3.sh
+++ AdmUsuarioPrueba3.sh
@@ -2,5 +2,10 @@
 #espacio para probar casos individuales
 #TRABAJANDO EN:
 : '
-nada 
+probar una cosita
 '
+
+mapfile -t listaGrupos < <(getent group | awk -F: '$3 >= 1000 && $3 < 60000 {print $1}')
+echo "${listaGrupos[@]}"
+mapfile listaGrupos < <(getent group | awk -F: '$3 >= 1000 && $3 < 60000 {print $1}')
+echo "${listaGrupos[@]}"
